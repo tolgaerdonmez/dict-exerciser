2bd4a53e5af6d430285e4abe59fbd1db95605ab0
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2019-02-23 15:22:42 +0300

    neater code style

--- dict_exerciser_app.py
+++ dict_exerciser_app.py
@@ -3,10 +3,12 @@ __version__ = "1.0"
 from PyQt5.QtWidgets import QWidget,QApplication,QPushButton,QLabel,QLineEdit,QHBoxLayout,QVBoxLayout,QComboBox,QMessageBox,QStyleFactory
 from PyQt5 import QtGui
 from PyQt5 import QtCore
-import sys
-import requests
-import sqlite3
-import os
+import sys, requests, sqlite3, os, json
+
+locales = None
+# getting locales.json
+with open('locales.json') as json_file:
+    locales = json.loads(json_file.read())
 
 class app_dict_login(QWidget):
 
@@ -80,33 +82,23 @@ class app_dict_login(QWidget):
     
     def set_lang(self,select):
         #lang_choice = self.sender().objectName()
-        #print(lang_choice)
-        def turkish():
-            self.usernameLabel.setText('Kullanıcı Adı: ')
-            self.passwordLabel.setText('Şifre: ')
-            self.log_in.setText('Giriş Yap')
-            self.register.setText('Üye Ol')
-            self.setWindowTitle('Kelime Çalışması & Çeviri | Giriş Yap')
-        def english():
-            self.usernameLabel.setText('Username: ')
-            self.passwordLabel.setText('Password: ')
-            self.log_in.setText('Log in')
-            self.register.setText('Register')
-            self.setWindowTitle('Word Exerciser & Translate | Log In')
-        def german():
-            self.usernameLabel.setText('Benutzername: ')
-            self.passwordLabel.setText('Passwort: ')
-            self.log_in.setText('Anmelden')
-            self.register.setText('Registrieren')
-            self.setWindowTitle('Wörter üben & Übersetzen | Anmelden')
+        local = locales["login_panel"]
         if select == 'tr':
-            turkish()
+            local = local[0]
         elif select == 'en':
-            english()
+            local = local[1]
         elif select == 'de':
-            german()
+            local = local[2]
+        
+        self.usernameLabel.setText(local["username"])
+        self.passwordLabel.setText(local["password"])
+        self.log_in.setText(local["login"])
+        self.register.setText(local["register"])
+        self.setWindowTitle(local["window_title"])
 
+        #setting the current lang
         self.current_lang = select
+
     def sys_login(self):
         username = self.usernameInput.text()
         password = self.passwordInput.text()
@@ -134,34 +126,18 @@ class app_dict_login(QWidget):
                 check_if_same = 'exists'
                 break
         if new_username == '':
-            msg = ["Lütfen geçerli bir kullanıcı adı giriniz !",'Please enter a valid username !',"Schreib bitte ein valider Benutzername !"]
-            if self.current_lang == 'tr':
-                self.login_output.setText(msg[0])
-            elif self.current_lang == 'en':
-                self.login_output.setText(msg[1])
-            elif self.current_lang == 'de':
-                self.login_output.setText(msg[2])
-            
+            msg = {"tr":"Lütfen geçerli bir kullanıcı adı giriniz !","en":"Please enter a valid username !","de":"Schreib bitte ein valider Benutzername !"}
+            self.login_output.setText(msg[self.current_lang])    
         elif check_if_same == 'exists':
-            msg = ["Zaten böyle bir kullanıcı var\nlütfen başka bir kullanıcı adı deneyiniz",
-                   'That username already exists\nplease try a new one',"Es gibt selben Benutzername\nVersucht bitte ein ander !"]
-            if self.current_lang == 'tr':
-                self.login_output.setText(msg[0])
-            elif self.current_lang == 'en':
-                self.login_output.setText(msg[1])
-            elif self.current_lang == 'de':
-                self.login_output.setText(msg[2])
+            msg = {"tr":"Zaten böyle bir kullanıcı var\nlütfen başka bir kullanıcı adı deneyiniz",
+                   "en":"That username already exists\nplease try a new one","de":"Es gibt selben Benutzername\nVersucht bitte ein ander !"}
+            self.login_output.setText(msg[self.current_lang])
         else:
             practice_words = str()
             self.cursor.execute("INSERT INTO userdata Values(?,?,?)",(new_username,new_password,practice_words))
             self.connection.commit()
-            msg = ["Başarıyla üye oldunuz !",'Successfully registered !',"Du hast mit Erfolg registriert !"]
-            if self.current_lang == 'tr':
-                self.login_output.setText(msg[0])
-            elif self.current_lang == 'en':
-                self.login_output.setText(msg[1])
-            elif self.current_lang == 'de':
-                self.login_output.setText(msg[2])
+            msg = {"tr":"Başarıyla üye oldunuz !","en":'Successfully registered !',"de":"Du hast mit Erfolg registriert !"}
+            self.login_output.setText(msg[self.current_lang])
 
 class app_word_translate(QWidget):
     def __init__(self,user_data,lang):
@@ -221,15 +197,12 @@ class app_word_translate(QWidget):
         self.h_box_output = QHBoxLayout()
         
         self.h_box_combo_box.addWidget(self.lang_one)
-        self.lang_one.addItem('Türkçe')
-        self.lang_one.addItem('English')
-        self.lang_one.addItem('Deutsch')
-        self.lang_one.addItem('Français')
+        langs = ['Türkçe','English','Deutsch','Français']
+        for lang in langs:
+            self.lang_one.addItem(lang)
         self.h_box_combo_box.addWidget(self.lang_two)
-        self.lang_two.addItem('Türkçe')
-        self.lang_two.addItem('English')
-        self.lang_two.addItem('Deutsch')
-        self.lang_two.addItem('Français')
+        for lang in langs:
+            self.lang_two.addItem(lang)
         self.v_box.addWidget(self.user_label)
         self.v_box.addLayout(h_box_label)
         self.v_box.addLayout(self.h_box_combo_box)
@@ -278,58 +251,34 @@ class app_word_translate(QWidget):
 
         return h_box
     def set_lang(self,lang):
-        def turkish():
-            self.setWindowTitle('Kelime Çalışması & Çeviri')
-            self.logout_btn.setText('Çıkış yap')
-            self.user_label.setText("Merhaba! {}".format(self.user_data[0].capitalize()))
-            self.go_practice.setText('Kelimelerini çalış !')
-            self.show_practice_list.setText('Çalışma listeni gör !')
-            self.translate_btn.setText('Çevir')
-            self.add_word_to_list.setText('Ekle')
-            self.add_word_to_list.setToolTip('Çalışma listene ekle !')
-            self.from_label.setText('   Neyden')
-            self.to_label.setText('Neye')
-        def english():
-            self.setWindowTitle('Word Practice & Translate')
-            self.logout_btn.setText('Log out')
-            self.user_label.setText("Hi! {}".format(self.user_data[0].capitalize()))
-            self.go_practice.setText('Practice Words !')
-            self.show_practice_list.setText('See Your Practice List')
-            self.translate_btn.setText('Translate')
-            self.add_word_to_list.setText('Add')
-            self.add_word_to_list.setToolTip('Add to your practice list !')
-            self.from_label.setText('   From')
-            self.to_label.setText('To')
-        def deutsch():
-            self.setWindowTitle('Wort Üben & übersetzen')
-            self.logout_btn.setText('Abmelden')
-            self.user_label.setText("Hallo! {}".format(self.user_data[0].capitalize()))
-            self.go_practice.setText('Üben Wörter !')
-            self.show_practice_list.setText('Sieh deine Liste !')
-            self.translate_btn.setText('Übersetzt')
-            self.add_word_to_list.setText('Hinzufügen')
-            self.add_word_to_list.setToolTip('Füg deine Liste hinzu !')
-            self.from_label.setText('   Von')
-            self.to_label.setText('Zu')
+        local = locales["main_panel"]
         if lang == 'tr':
-            turkish()
+            local = local[0]
         elif lang == 'en':
-            english()
+            local = local[1]
         elif lang == 'de':
-            deutsch()
+            local = local[2]
+
+        self.setWindowTitle(local["window-title"])
+        self.logout_btn.setText(local["logout"])
+        self.user_label.setText(local["user_label"].format(self.user_data[0].capitalize()))
+        self.go_practice.setText(local["go_practice"])
+        self.show_practice_list.setText(local["show_practice_list"])
+        self.translate_btn.setText(local["translate_btn"])
+        self.add_word_to_list.setText(local["add_word_to_list.text"])
+        self.add_word_to_list.setToolTip(local["add_word_to_list.tooltip"])
+        self.from_label.setText(local["from_label"])
+        self.to_label.setText(local["to_label"])
+
+        #setting current lang
         self.current_lang = lang
+
     def event_get_lang_choice(self):
-        langs = ['tr','en','de','fr']
         l1 = str()
         l2 = str()
         if self.lang_one.currentText() == self.lang_two.currentText():
-            msg = ["Aynı dilden aynı dile çevirilmiyor !","Can't translate from same language !","Kann nicht zu selben Sprache übersetzen !"]
-            if self.current_lang == 'tr':
-                msg_box = QMessageBox.warning(self, '!!!', msg[0] , QMessageBox.Ok)
-            elif self.current_lang == 'en':
-                msg_box = QMessageBox.warning(self, '!!!', msg[1] , QMessageBox.Ok)
-            elif self.current_lang == 'de':
-                msg_box = QMessageBox.warning(self, '!!!', msg[2] , QMessageBox.Ok)
+            msg = {"tr":"Aynı dilden aynı dile çevirilmiyor !","en":"Can't translate from same language !","de":"Kann zu selben Sprache nicht übersetzen !"}
+            QMessageBox.warning(self, '!!!', msg[self.current_lang] , QMessageBox.Ok)
         else:
             if self.lang_one.currentText() == "Türkçe":
                 l1 = 'tr'
@@ -342,9 +291,10 @@ class app_word_translate(QWidget):
                 l2 = self.lang_two.currentText()[0:2].lower()
             lang = l1 + '-' + l2
             self.get_trans(lang)
+
     def get_trans(self,lang):
         base_url = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180424T202020Z.dbeff593adc28b99.4ee8cc89b87584c797a29178d75bef63add3a05a&'
-        custom_url = 'lang={}&text={}'.format(lang,self.word.text())
+        custom_url = f'lang={lang}&text={self.word.text()}'
         dict_val = requests.get(base_url + custom_url).json()
         
         try:
@@ -356,13 +306,9 @@ class app_word_translate(QWidget):
                 self.h_box_output.addStretch()
                 self.check_first += 1
         except IndexError:
-            msg = ["'{}' diye bir kelime yok ! ","There is no word here such as '{}'","Es gibt keinen Wort heißt '{}' !"]
-            if self.current_lang == 'tr':
-                msg_box = QMessageBox.warning(self, '!!!', msg[0].format(self.word.text()), QMessageBox.Ok)
-            elif self.current_lang == 'en':
-                msg_box = QMessageBox.warning(self, '!!!', msg[1].format(self.word.text()), QMessageBox.Ok)
-            elif self.current_lang == 'de':
-                msg_box = QMessageBox.warning(self, '!!!', msg[2].format(self.word.text()), QMessageBox.Ok)
+            msg = {"tr":"'{}' diye bir kelime yok ! ","en":"There is no word here such as '{}'","de":"Es gibt keinen Wort, der '{}' heißt !"}
+            QMessageBox.warning(self, '!!!', msg[self.current_lang].format(self.word.text()), QMessageBox.Ok)
+    
     def event_add_to_list(self):
         text = self.word.text() + ',' + self.trans_output.text()
         #CHECKING IF EMPTY OR NOT!
@@ -379,25 +325,16 @@ class app_word_translate(QWidget):
         try:
             self.see_prac_list = practice_list(self.user_data,self.current_lang)
         except IndexError:
-            msg = ["Listen boş ! ","Your List is Empty !","Deine Liste ist leer !"]
-            if self.current_lang == 'tr':
-                msg_box = QMessageBox.warning(self, '!!!', msg[0], QMessageBox.Ok)
-            elif self.current_lang == 'en':
-                msg_box = QMessageBox.warning(self, '!!!', msg[1], QMessageBox.Ok)
-            elif self.current_lang == 'de':
-                msg_box = QMessageBox.warning(self, '!!!', msg[2], QMessageBox.Ok)
+            msg = {"tr":"Listen boş ! ","en":"Your List is Empty !","de":"Deine Liste ist leer !"}
+            QMessageBox.warning(self, '!!!', msg[self.current_lang], QMessageBox.Ok)
             
     def event_go_practice(self):
         try:
             self.go_practice_words = word_list_practice(self.user_data,self.current_lang)
         except IndexError:
-            msg = ["Listen boş ! ","Your List is Empty !","Deine Liste ist leer !"]
-            if self.current_lang == 'tr':
-                msg_box = QMessageBox.warning(self, '!!!', msg[0], QMessageBox.Ok)
-            elif self.current_lang == 'en':
-                msg_box = QMessageBox.warning(self, '!!!', msg[1], QMessageBox.Ok)
-            elif self.current_lang == 'de':
-                msg_box = QMessageBox.warning(self, '!!!', msg[2], QMessageBox.Ok)
+            msg = {"tr":"Listen boş ! ","en":"Your List is Empty !","de":"Deine Liste ist leer !"}
+            QMessageBox.warning(self, '!!!', msg[self.current_lang], QMessageBox.Ok)
+
     def logout_to_main_app(self):
         self.login_screen = app_dict_login(self.current_lang)
         self.close()
@@ -410,7 +347,6 @@ class app_word_translate(QWidget):
                     widget.deleteLater()
                 else:
                     self.clearLayout(item.layout())
-
                     
 class practice_list(QWidget):
     
@@ -468,21 +404,14 @@ class practice_list(QWidget):
         self.set_lang(self.current_lang)
         self.show()
     def set_lang(self,lang):
-        def turkish():
-            self.setWindowTitle('Listene Bak !')
-            self.clear_list.setText('Listeyi temizle')
-        def english():
-            self.setWindowTitle('See your List !')
-            self.clear_list.setText('Clear List')
-        def deutsch():
-            self.setWindowTitle('Sieh deine Liste !')
-            self.clear_list.setText('Lösch deine Liste !')           
+        local = locales["practice_list_panel"]
         if lang == 'tr':
-            turkish()
+            local = local[0]
         elif lang == 'en':
-            english()
+            local = local[1]
         elif lang == 'de':
-            deutsch()
+            local = local[1]
+
     def event_clear_list(self):
         self.clearLayout(self.v_box)
         self.cursor.execute("UPDATE userdata SET user_practice_words = '' WHERE username = ? AND password = ?",(self.user_data[0],self.user_data[1]))
@@ -494,11 +423,11 @@ class practice_list(QWidget):
         i = 0
         words = list()
         
-##        while len(user_practice_words) > i:
-##            add = (user_practice_words[i],user_practice_words[i+1])
-##            words.append(add)
-##            i += 2
-##        w = dict(enumerate(words))
+        ##        while len(user_practice_words) > i:
+        ##            add = (user_practice_words[i],user_practice_words[i+1])
+        ##            words.append(add)
+        ##            i += 2
+        ##        w = dict(enumerate(words))
         
         
         btn = self.sender().objectName()
@@ -534,6 +463,7 @@ class practice_list(QWidget):
                     widget.deleteLater()
                 else:
                     self.clearLayout(item.layout())
+
 class word_list_practice(QWidget):
     
     def __init__(self,user_data,lang):
@@ -586,21 +516,17 @@ class word_list_practice(QWidget):
         self.set_lang(self.current_lang)
         self.show()
     def set_lang(self,lang):
-        def turkish():
-            self.setWindowTitle('Çalış !')
-            self.check_words.setText('Kontrol Et')
-        def english():
-            self.setWindowTitle('Practice !')
-            self.check_words.setText('Check')
-        def deutsch():
-            self.setWindowTitle('Üben !')
-            self.check_words.setText('Kontrollieren')            
+        local = locales["practice_panel"]
         if lang == 'tr':
-            turkish()
+            local = local[0]
         elif lang == 'en':
-            english()
+            local = local[1]
         elif lang == 'de':
-            deutsch()
+            local = local[2]
+
+        self.setWindowTitle(local["window-title"])
+        self.check_words.setText(local["check_list"])
+
     def event_check_words(self):
         self.cursor.execute('SELECT user_practice_words FROM userdata WHERE username = ? AND password = ?',(self.user_data[0],self.user_data[1]))
         data = self.cursor.fetchall()[0]
@@ -636,15 +562,15 @@ class word_list_practice(QWidget):
     def del_selected_item(self,item,w):
         self.cursor.execute('SELECT user_practice_words FROM userdata WHERE username = ? AND password = ?',(self.user_data[0],self.user_data[1]))
         data = self.cursor.fetchall()[0]
-        user_practice_words = data[0].split(',')
-        
-##        i = 0
-##        words = list()
-##        while len(user_practice_words) > i:
-##            add = (user_practice_words[i],user_practice_words[i+1])
-##            words.append(add)
-##            i += 2
-##        w = dict(enumerate(words))
+
+        # user_practice_words = data[0].split(',')
+        #        i = 0
+        #        words = list()
+        #        while len(user_practice_words) > i:
+        #            add = (user_practice_words[i],user_practice_words[i+1])
+        #            words.append(add)
+        #            i += 2
+        #        w = dict(enumerate(words))
         
         btn = item.objectName()
         number = btn.split("|")[1]
@@ -676,8 +602,8 @@ class word_list_practice(QWidget):
                 else:
                     self.clearLayout(item.layout())
 
-        
-app = QApplication(sys.argv)
-app.setStyle(QStyleFactory.create("fusion"))
-app_dict = app_dict_login()
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create("fusion"))
+    app_dict = app_dict_login()
+    sys.exit(app.exec_())
